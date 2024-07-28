710e439e53031f956240e62cf1dbe1f1d2f6910e
AYUSHMANPAUDEL <mrdark2064@gmail.com>
2024-07-28 09:04:19 +0545

    blog adding feature added

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,6 @@ from django.urls import path , include
 from . import views
 
 urlpatterns = [
-    path('',views.home)
+    path('',views.home),
+    path('blogs/<str:blog_name>/', views.blog_detail, name='blog_detail'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render , HttpResponse
-
+from django.shortcuts import render , HttpResponse ,  get_object_or_404
+from .models import blogs
+from django.http import HttpResponse, Http404
 # Create your views here.
 def home(request):
     return render(request,"home/index.html")
+
+def blog_detail(request, blog_name):
+    try:
+        post = blogs.objects.get(title=blog_name)
+    except blogs.DoesNotExist:
+        raise Http404("Blog post does not exist")
+    return render(request,"home/blogs_preview.html",{"post": post})
